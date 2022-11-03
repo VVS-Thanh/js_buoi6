c5b2058b907df8e6a06e38b2910ad89c27c60f93
VVS-Thanh <btthanh11@gmail.com>
2022-11-03 15:57:50 +0700

    Update_test

--- bai1.js
+++ bai1.js
@@ -6,17 +6,16 @@ var Arr = [];
 
 //Kiểm tra Object
 if (typeof object !== "object") {
-  console.log(`${object} không phải là Object. Mời nhập lại`);
+  console.log(`${object} không phải là Object. Mời nhập lại.`);
 } else if (Object.keys(object).length == 0) {
   console.log("Object đã cho là Object rỗng");
 } else console.log("Object đã cho không phải là Object rỗng.");
 
-console.log(Object.keys(object).length);
+// console.log(typeof Object.keys(object).length); => trả về number
 // Kiểm tra mảng
 if (Array.isArray(Arr) != true) {
   console.log(Arr + "không phải là mảng. Mời thử lại.");
 } else if (Arr.length == 0) {
   console.log("Mảng đã cho là mảng rỗng");
 } else console.log("Mảng đã cho không phải là mảng rỗng");
-
 console.log(typeof 0);
